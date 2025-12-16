ef0355e128f8e87ea5ac4e0b1b22fa09935dbf50
rtlnp <rtlnpsekai@gmail.com>
2025-12-16 18:17:16 +0800

    修改了README

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/vue-java-pinia/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
